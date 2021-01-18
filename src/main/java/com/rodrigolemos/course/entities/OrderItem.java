d180e09c906c24bc47f468716c17f2ad0848b75d
Rodrigo <rodrigolemosrl@hotmail.com>
2021-01-18 19:58:45 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/rodrigolemos/course/entities/OrderItem.java
+++ src/main/java/com/rodrigolemos/course/entities/OrderItem.java
@@ -14,13 +14,13 @@ import com.rodrigolemos.course.entities.pk.OrderItemPK;
 public class OrderItem implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@EmbeddedId
 	private OrderItemPK id = new OrderItemPK();
 
 	private Integer quantity;
 	private Double price;
-	
+
 	public OrderItem() {
 	}
 
@@ -31,20 +31,20 @@ public class OrderItem implements Serializable {
 		this.quantity = quantity;
 		this.price = price;
 	}
-	
+
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
-	
+
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-	
+
 	public Product getProduct() {
 		return id.getProduct();
 	}
-	
+
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
@@ -89,5 +89,5 @@ public class OrderItem implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 }
