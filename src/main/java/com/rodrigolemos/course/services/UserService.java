f3dfa12e518e599053299f9e24a5c7c128a4dac3
Rodrigo <rodrigolemosrl@hotmail.com>
2021-01-20 00:12:15 -0300

    User delete

--- src/main/java/com/rodrigolemos/course/services/UserService.java
+++ src/main/java/com/rodrigolemos/course/services/UserService.java
@@ -11,21 +11,25 @@ import com.rodrigolemos.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
 	}
 
 }
